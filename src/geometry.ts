55ecf86f7d473eb31c8526d40816754e32a88f4f
mhimavamshi <himavamshi1212@gmail.com>
2026-06-27 17:00:31 +0530

    add a mesh interface

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,10 +1,22 @@
 interface Point2d {
-    x: number;
-    y: number;
+    x: number,
+    y: number
 }
 
 interface Point3d extends Point2d {
-    z: number;
+    z: number
 }
 
-export type { Point2d, Point3d };
+interface Triangle {
+    a: number,
+    b: number,
+    c: number
+}
+
+interface Mesh {
+    vertices: Array<Point3d>,
+    triangles: Array<Triangle>
+}
+
+
+export type { Point2d, Point3d, Triangle, Mesh };
