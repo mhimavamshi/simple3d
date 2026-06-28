5399762791823d9916343d9ed92ce153c18a8646
mhimavamshi <himavamshi1212@gmail.com>
2026-06-28 18:38:25 +0530

    declare the flow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Screen } from "./screen";
+import { Pipeline } from "./pipeline";
 import { SObject, ObjectManager } from "./object";   
+import { ObjectTransforms } from "./objectTransforms";  
+import { Renderer } from "./renderer";  
+import { Camera3D } from "./camera";    
 import { EventManager, type EventInput, type EventHandler } from "./events";    
 import { assertNonNull } from "./utils";
 
@@ -14,6 +18,9 @@ assertNonNull(ctx);
 
 const objectmanager = new ObjectManager();
 const eventmanager = new EventManager();
+const camera = new Camera3D(canvas.height, canvas.width);
+const pipeline = new Pipeline();
+const renderer = new Renderer(canvas.height, canvas.width);
 
 eventmanager.hookToBrowser({canvas: canvas});
 
@@ -36,13 +43,18 @@ const echoevents = new EchoHandler();
 
 eventmanager.register(echoevents);
 
-const cube = new SObject({x: 10, y: 10, z: 30}, {vertices: [{x: 1, y: 1, z: 100}], triangles: [{a: 0, b: 0, c: 0}]}); // its wrong but you get the point
-objectmanager.add("cube", cube);
+// const cube = new SObject({x: 10, y: 10, z: 30}, {vertices: [{x: 1, y: 1, z: 100}], triangles: [{a: 0, b: 0, c: 0}]}); // its wrong but you get the point
+// objectmanager.add("cube", cube);
 
-
-function loop(ctx: CanvasRenderingContext2D, delta: number) {
-
-}
+function loop(delta: number) {
+    eventmanager.dispatch();
+    // any physics updates handling the objects, that might use delta
+    // for now just object transforms
+    // objectmanager.applyToAll(ObjectTransforms.rotate);
+    let renderdata = pipeline.in(objectmanager, camera);
+    const imagedata = renderer.render(renderdata);
+    // function that puts image data on the screen
+}   
 
 const screen = new Screen(canvas, ctx);
 screen.onFrame(loop);
