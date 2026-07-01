5f4027bafc13e35ac7016904405c3b1340e7666a
mhimavamshi <himavamshi1212@gmail.com>
2026-07-01 15:32:49 +0530

    let camera own aspect and focal length

--- src/camera.ts
+++ src/camera.ts
@@ -7,17 +7,29 @@ class Camera3D {
     fov: Radians;
     viewportWidth: number;
     viewportHeight: number;
+    focalLength: number;
+    aspect: number;
     
     constructor(width: number, height: number) {
         this.pos = {x: 0, y: 0, z: 0};
         this.rotation = {pitch: 0, yaw: 0, roll: 0};
-        this.fov = Math.PI / 2;
+        this.fov = Math.PI / 4;
         this.viewportWidth = width;
         this.viewportHeight = height;
+
+        this.focalLength = (this.viewportHeight / 2) / Math.tan(this.fov/2);
+        this.aspect = this.viewportWidth / this.viewportHeight;
     }
 
-    setFov(fov: Degrees) {
-        this.fov = degreesToRadians(fov); 
+    setFov(fov: Radians) {
+        this.fov = fov;
+        this.recomputeProjection();
+    }
+
+    private recomputeProjection() {
+        this.aspect = this.viewportWidth / this.viewportHeight;
+        this.focalLength =
+            (this.viewportHeight / 2) / Math.tan(this.fov / 2);
     }
 
 }
