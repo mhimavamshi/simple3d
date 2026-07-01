c74477a93fe3d7080a6feb307aa225b9caeba469
mhimavamshi <himavamshi1212@gmail.com>
2026-07-01 15:31:33 +0530

    add pivot-based mesh rotation transforms, rotateZ

--- src/objectTransforms.ts
+++ src/objectTransforms.ts
@@ -8,35 +8,26 @@ interface RotationContext extends BaseContext {
     angularSpeed: number
 }
 
-
 function rotateX(object: SObject, ctx: RotationContext) {
     const angle = ctx.angularSpeed * ctx.delta;
-
-    console.log(object.pivot);
 
     const c = Math.cos(angle);
     const s = Math.sin(angle);
 
-    const pivot = object.computeCenter(object.mesh);
-    console.log("before", object.computeCenter(object.mesh));
+    const pivot = object.pivot;
 
     for (const vertex of object.mesh.vertices) {
-        // Translate to pivot
         const x = vertex.x - pivot.x;
         const y = vertex.y - pivot.y;
         const z = vertex.z - pivot.z;
 
-        // Rotate around X
         const ry = y * c - z * s;
         const rz = y * s + z * c;
 
-        // Translate back
         vertex.x = x + pivot.x;
         vertex.y = ry + pivot.y;
         vertex.z = rz + pivot.z;
     }
-    console.log("after", object.computeCenter(object.mesh));
-
 }
 
 function rotateY(object: SObject, ctx: RotationContext) {
@@ -44,7 +35,25 @@ function rotateY(object: SObject, ctx: RotationContext) {
 }
 
 function rotateZ(object: SObject, ctx: RotationContext) {
-    
+    const angle = ctx.angularSpeed * ctx.delta;
+
+    const c = Math.cos(angle);
+    const s = Math.sin(angle);
+
+    const pivot = object.pivot;
+
+    for (const vertex of object.mesh.vertices) {
+        const x = vertex.x - pivot.x;
+        const y = vertex.y - pivot.y;
+        const z = vertex.z - pivot.z;
+
+        const rx = x * c - y * s;
+        const ry = x * s + y * c;
+
+        vertex.x = rx + pivot.x;
+        vertex.y = ry + pivot.y;
+        vertex.z = z + pivot.z;
+    }
 }
 
 export { type RotationContext, rotateX, rotateY, rotateZ };
